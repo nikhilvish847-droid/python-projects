2089bbcd5debb4d00a65ca516e0b345d9b493ada
nikhilvish847-droid <nikhil.vish847@gmail.com>
2025-10-18 23:39:06 +0530

    Update news via api .py

--- news via api .py
+++ news via api .py
@@ -1,9 +1,8 @@
-#importing a library which are going to use#
 import requests as r
 import json
 
 topic=["politics","crime","cricket","trading"]
-#creating a greeting function#
+#creating a greeting function
 def greet(fx):
     def mfx():
         print("WELCOME TO NIKHIL NEWS, HERE YOU WILL GET DAILY REFRASHING NEWS!")
@@ -13,22 +12,47 @@ def greet(fx):
     return mfx
 @greet
 
-#creating a function to fetch news#
+#creting a functiont to fetch news via api
 def modify():
+    print(f"press 1 for news related to {topic[0]}\npress 2 for news related to {topic[1]}\npress 3 for news related to {topic[2]}\npress 4 for news related to {topic[3]}")
+    option=int(input("enter your option: "))
 
-    # taking a topic as input from user
-    topic=input("news of which topic you want ?")
-    
-    #calling via api from news.api
-    url=f"https://newsdata.io/api/1/latest?apikey=pub_d0d4187901f341549754d8eccd8c4ff9&q={topic}&language=en&category={topic}&country=in&timezone=Asia/Almaty"
-    news=r.get(url)
-    news=json.loads(news.text)
-    
-    #extracting data from dictionary
-    for article in news["results"]: 
-        print(article["title"])
-        print(article["description"])
-        print("------------------------------------------------------------------")
-
-#calling function
+    #using a matching fetaure#
+    match option:
+        case _ if option==1:
+            #callig a api from  news.api
+            url=f"https://newsdata.io/api/1/latest?apikey=pub_d0d4187901f341549754d8eccd8c4ff9&q={topic[0]}&language=en&category={topic[0]}&country=in&timezone=Asia/Almaty"
+            news=r.get(url)
+            news=json.loads(news.text)
+            #extracting data from dictionary
+            for article in news["results"]: 
+                print(article["title"])
+                print(article["description"])
+                print("------------------------------------------------------------------")
+        case _ if option==2:
+            url=f"https://newsdata.io/api/1/latest?apikey=pub_d0d4187901f341549754d8eccd8c4ff9&q={topic[1]}&language=en&category={topic[1]}&country=in&timezone=Asia/Almaty"
+            news=r.get(url)
+            news=json.loads(news.text)
+            for article in news["results"]: 
+                print(article["title"])
+                print(article["description"])
+                print("------------------------------------------------------------------")
+        case _ if option==3:
+            url=f"https://newsdata.io/api/1/latest?apikey=pub_d0d4187901f341549754d8eccd8c4ff9&q={topic[2]}&language=en&category={topic[2]}&country=in&timezone=Asia/Almaty"
+            news=r.get(url)
+            news=json.loads(news.text)
+            for article in news["results"]: 
+                print(article["title"])
+                print(article["description"])
+                print("------------------------------------------------------------------")
+        case _ if option==4:
+            url=f"https://newsdata.io/api/1/latest?apikey=pub_d0d4187901f341549754d8eccd8c4ff9&q={topic[3]}&language=en&category={topic[3]}&country=in&timezone=Asia/Almaty"
+            news=r.get(url)
+            news=json.loads(news.text)
+            for article in news["results"]: 
+                print(article["title"])
+                print(article["description"])
+                print("------------------------------------------------------------------")
+        case _ :
+            print("invalid option")
 modify()
