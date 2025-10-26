072c9dc2fe3c025cd41c582b07385ef1730f040b
nikhilvish847-droid <nikhil.vish847@gmail.com>
2025-10-26 18:42:17 +0530

    adding more conditions to validate email

--- validateemail.py
+++ validateemail.py
@@ -1,19 +1,32 @@
-
+sign=["_",".","@"]
+k=0
 email=input("Enter your email address: ")
 if len(email)>=6:
     if email[0].isalpha():
-        if email.count("@")==1:
-            if email.isspace():
-                print("invalid email")
-            else:
-                if email[-3]=="." ^ email[-4]==".":
-                    pass
+        if email.count("@")==1: 
+            if email[-3]=="." or email[-4]==".":
+                for i in email:
+                    if i.isspace():
+                        print("invalid 2 email")
+                        break
+                    elif i in sign or i.isdigit():
+                        continue
+                    elif i.isalpha():
+                        if i==i.upper():
+                            print("invalid email:email has some uppercase letter.")
+                            break
+                        else:
+                            continue
+                    else:
+                        print("invalid email: this email has some irrelevent operator.")
+                        break 
                 else:
-                    print("invalid email")
+                    print("right email")                 
+            else:
+                print("invalid email:email should end with .com or .in .")
         else:
-            print("invalid email")
+            print("invalid email:email has some irrelevent operator.")
     else:
-        print("invalid email")
-
+        print("invalid email:first letter is not a alphabet.")
 else:
-    print("invalid email")
+    print("invalid email:minimum character should be 6.")
